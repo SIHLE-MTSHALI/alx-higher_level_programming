42c8552373866e40bf2f770897fe7e7a2793c332
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-02 16:12:22 -0800

    03 v2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-# Print the lowercase ASCII alphabet, skipping 'q' and 'e', in one line
+# Print the lowercase ASCII, skipping 'q' and 'e', in one line
 print("{}".format(''.join(chr(i) for i in range(97, 123) if chr(i) not in 'qe')), end='')

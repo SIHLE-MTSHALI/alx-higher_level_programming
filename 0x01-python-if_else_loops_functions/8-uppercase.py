67276385fa76493f9bda3f7dba0cead59553a0ac
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-02 16:41:48 -0800

    08

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """Print a string in uppercase."""
+    uppercase_str = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_str += chr(ord(char) - 32)
+        else:
+            uppercase_str += char
+    print("{}".format(uppercase_str))

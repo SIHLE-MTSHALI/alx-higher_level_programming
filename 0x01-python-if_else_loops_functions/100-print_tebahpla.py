aa27e4e2fdd1f90235d75ac1d779967225d0b892
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-02 17:41:54 -0800

    14 fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 # Print ASCII alphabet in reverse, alternating lowercase and uppercase
-print("{}".format(''.join(chr(i) if i % 2 != 0 else chr(i - 32) 
+print("{}".format(''.join(chr(i) if i % 2 == 0 else chr(i - 32)
                           for i in range(122, 96, -1))), end='')

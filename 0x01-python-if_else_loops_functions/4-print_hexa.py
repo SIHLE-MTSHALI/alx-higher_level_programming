4f08cf55c66e993fef4d81e9ad859b9eef924d1f
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-02 16:30:15 -0800

    04

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 # Print numbers from 0 to 98 in decimal and hexadecimal
-print("\n".join("{} = {}".format(i, hex(i)) for i in range(99)), end='')
+print("\n".join("{} = {}".format(i, hex(i)) for i in range(99)), end='\n')

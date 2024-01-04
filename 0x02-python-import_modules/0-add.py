c24157d19adb827bb89b9c3fe7d322382db548dc
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-04 13:35:12 -0800

    00 fixed 5

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if _name_ == "_main_":
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+print(f"{a} + {b} = {add(a, b)}")

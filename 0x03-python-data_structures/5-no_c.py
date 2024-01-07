976b45ad406113c975a7b8869c58ada04f4394a1
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-07 13:47:13 -0800

    05 fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,6 @@
 def no_c(my_string):
     """
     Removes all characters 'c' and 'C' from a string.
+    Returns the new string.
     """
-    return ''.join(ch for ch in my_string if ch not in ['c', 'C'])
+    return ''.join(char for char in my_string if char not in ['c', 'C'])

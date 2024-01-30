ad562a2a732adf4a79b46d33b7c056ae465a9527
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-30 12:38:00 -0800

    25-31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    This class allows creating objects with a limited set of attributes.
+    Specifically, only an attribute 'first_name' is allowed.
+    """
     __slots__ = ['first_name']

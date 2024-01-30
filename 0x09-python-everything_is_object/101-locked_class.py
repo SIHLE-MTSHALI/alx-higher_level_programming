f5571e1d0d5068083282f28d035edd00558533a1
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-01-30 13:32:55 -0800

    30 fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
+
+"""
+LockedClass: A class that enforces strict attribute control.
+
+(... rest of the file-level docstring ...)
+"""
+
+
 class LockedClass:
-    __slots__ = ('first_name',)
+    """
+    Provides a restricted attribute model with only "first_name" allowed.
+    """
+
+    __slots__ = ("first_name",)  # Tuple defining permitted attributes
 
     def __setattr__(self, name, value):
-        if name == 'first_name':
-            super().__setattr__(name, value)
+        """
+        Controls attribute setting behavior.
+
+        Args:
+            name (str): Name of the attribute to set.
+            value: Value to assign to the attribute.
+
+        Raises:
+            AttributeError: If the attribute name is not "first_name".
+        """
+
+        if name == "first_name":
+            super().__setattr__(name, value)  # Allow setting "first_name"
         else:
             raise AttributeError(
                 f"'LockedClass' object has no attribute '{name}'"

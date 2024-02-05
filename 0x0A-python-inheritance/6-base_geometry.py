857934771b7a8b0bcd01d90b8d7c87e7dc8cb62c
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-02-05 13:25:36 -0800

    06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry enhances the BaseGeometry class with a public instance
+method named area.
+"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry with public instance method area.
+    """
+    
+    def area(self):
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")

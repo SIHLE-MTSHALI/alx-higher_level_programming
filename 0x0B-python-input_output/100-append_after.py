aaf10a5457ea1702c14f4ee9aa80baf1a81279be
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-02-06 11:51:07 -0800

    13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module to insert a line of text to a file, after each line containing
+a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to file after each line containing a string."""
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)

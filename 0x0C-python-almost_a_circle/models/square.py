3fb5d03f715b0d6b4dd49b4491c941d4043d3f44
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-02-12 12:50:38 -0800

    10 fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,34 +1,17 @@
+#!/usr/bin/python3
+"""
+Module inheriting from Rectangle
+"""
 from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    Represents a square, inheriting from Rectangle.
-    
-    Attributes:
-        size (int): The size of the square's sides.
-        x (int): The x-coordinate of the square.
-        y (int): The y-coordinate of the square.
-        id (int): The square's identifier.
-    """
+    """Represents a square, inherits from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a new Square.
-        
-        Args:
-            size (int): The size of the square's sides.
-            x (int, optional): The x-coordinate of the square. Defaults to 0.
-            y (int, optional): The y-coordinate of the square. Defaults to 0.
-            id (int, optional): The square's identifier. Defaults to None.
-        """
+        """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
-        Returns the string representation of the Square instance.
-        
-        Returns:
-            str: String representation of the Square.
-        """
+        """Return the string representation of the Square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"

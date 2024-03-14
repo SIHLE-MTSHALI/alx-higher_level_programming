a0591d387b780e27729c78de11c5e3807bbf506f
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-03-13 18:16:40 -0700

    18. Temperatures #0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
--- Display average temperature by city in descending order
-SELECT city, AVG(temperature) AS avg_temp
+-- Displays average temperature by city ordered by temperature desc
+SELECT city, AVG(value) AS avg_temp 
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;

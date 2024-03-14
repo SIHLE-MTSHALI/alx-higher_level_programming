755f2c292705e85b1e15ba7322615838c4879daf
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-03-13 18:13:33 -0700

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,5 @@
--- Select the database hbtn_0c_0
+-- Convert database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-
--- Convert table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert field to UTF8 (this step is actually redundant since the table conversion above will also convert all fields)
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 

328e7e950dcb22029cde97918870f05cdef630dd
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-03-13 17:30:55 -0700

    14 and 15 fix

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,14 @@
 -- 15-comedy_only.sql
 SELECT tv_shows.title
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-
-WHERE tv_genres.name = Comedy
+WHERE tv_shows.id IN (
+    SELECT show_id
+    FROM tv_show_genres
+    WHERE genre_id = (
+        SELECT id 
+        FROM tv_genres 
+        WHERE name = 'Comedy'
+    )
+)
 ORDER BY tv_shows.title ASC;
-
 

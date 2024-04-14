050079b76dd29ee1e38bfde427672537d347304c
Sihle <sihle.mtshali8@gmail.com>
2024-04-14 15:11:03 -0700

    Corrected and finalized Task 14: Fetching cities by state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Script to list all City objects from the database hbtn_0e_14_usa"""
-
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa,
+formatted as <state name>: (<city id>) <city name>.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,17 +10,11 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        for city in state.cities:
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-
+    for city in session.query(City).join(State).order_by(City.id):
+        print(f"{city.state.name}: ({city.id}) {city.name}")
     session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Defines a State model"""
-
+"""
+Definition of the State class with a link to the MySQL table states.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from model_city import City
 
 Base = declarative_base()
 
-
 class State(Base):
-    """Represents a state for a MySQL database"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", backref="state", cascade="all, delete")

d602a2f80c00d16688eaf9523731bc0e92b41159
Sihle <sihle.mtshali8@gmail.com>
2024-04-14 15:15:24 -0700

    Corrected and finalized Task 14: Fetching cities by state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ Definition of the City class with a link to the MySQL table cities.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)

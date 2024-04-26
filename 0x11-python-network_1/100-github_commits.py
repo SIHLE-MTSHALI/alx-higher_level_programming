7742ed1f07f984bfc264d41f6896bde44cb48e36
Sihle <sihle.mtshali8@gmail.com>
2024-04-26 15:22:16 -0700

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""Script that lists 10 commits (from the most recent to oldest)
-of the repository "rails" by the user "rails""""
+
+# Importing necessary packages
 import requests
 import sys
 
+# Retrieve the repository name and owner name from command-line arguments
+repo_name = sys.argv[1]
+owner_name = sys.argv[2]
 
-if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(
-        sys.argv[2], sys.argv[1])
-    r = requests.get(url)
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+# Define the GitHub API URL for retrieving commits
+url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+# Send a GET request to retrieve the last 10 commits
+response = requests.get(url)
+
+# Parse the response as JSON
+json_response = response.json()
+
+# Display the last 10 commits in the specified format
+for commit in json_response[:10]:
+    sha = commit["sha"]  # Commit SHA
+    author_name = commit["commit"]["author"]["name"]  # Commit author name
+    print(f"{sha}: {author_name}")  # Display in '<sha>: <author name>' format

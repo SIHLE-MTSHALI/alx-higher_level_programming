7f9540522b52f61464e21d7cb32bb4ce2317dc58
Sihle <sihle.mtshali8@gmail.com>
2024-05-21 10:32:34 -0700

    101. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  } else {
+    console.log(error);
+  }
+});
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  request(characters[index], (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
